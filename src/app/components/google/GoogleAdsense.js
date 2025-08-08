deee0581413ec8a969f70a516d6f8d58ea23ae66
ezshine <ezshine@live.cn>
2025-08-08 11:00:55 +0700

    bugfix

--- src/app/components/google/GoogleAdsense.js
+++ src/app/components/google/GoogleAdsense.js
@@ -5,8 +5,12 @@ import Script from 'next/script';
 export default function GoogleAdsense() {
   return (
     <>
-      <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}`}
-    crossOrigin="anonymous"></Script>
+      <Script 
+        async 
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}`}
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+      />
     </>
   );
 }
