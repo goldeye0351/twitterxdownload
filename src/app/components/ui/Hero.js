107443e25dafabaec3b3488ed7e51f1c15526c95
Goldeye0351 <goldeye0351@gmail.com>
2025-10-13 18:16:22 +0800

    Update Hero.js

--- src/app/components/ui/Hero.js
+++ src/app/components/ui/Hero.js
@@ -1,19 +1,15 @@
 'use client';
-import { getTranslation } from '@/lib/i18n';
 import { Button, addToast } from '@heroui/react';
 import { RiDownloadLine } from '@remixicon/react';
 import { useRef } from 'react';
 
-export default function Hero({ locale = 'en', 
+export default function Hero({  
     downloadButtonLabel = 'Download', 
     downloadButtonIsLoading = false,
     remainApiCount = 0,
     onDownload = (url) => {}, 
     url = 'https://x.com/elonmusk/status/1853948745521439079'
 }) {
-    const t = function (key) {
-        return getTranslation(locale, key);
-    }
 
     const inputRef = useRef(null);
 
@@ -21,10 +17,10 @@ export default function Hero({ locale = 'en',
         <>
             <div className="text-center pt-16 pb-2">
                 <h1 className="text-5xl font-bold text-primary mb-2">
-                    {t('Download Twitter Video And ALL')}
+                    Download Twitter Video And ALL
                 </h1>
                 <p className="text-4xl text-subtext mb-8">
-                    {t('Free and No Registration Required')}
+                   Free and No Registration Required
                 </p>
                 <div className="max-w-xl mx-auto pt-6">
                     <div className="relative mb-6">
@@ -46,7 +42,7 @@ export default function Hero({ locale = 'en',
                                 });
                             }}
                             className="absolute right-2 top-1/2 -translate-y-1/2 px-3 py-1 bg-gray-700 text-gray-300 rounded-lg">
-                            {t('Paste')}
+                         Paste
                         </Button>
                     </div>
                     <Button
@@ -85,10 +81,10 @@ export default function Hero({ locale = 'en',
                         spinnerPlacement="end"
                         startContent={<RiDownloadLine />}
                         color="primary" className="text-lg py-6 px-20 rounded-full mb-3" >
-                        {t(downloadButtonLabel)}
+                        {downloadButtonLabel}
                     </Button>
                     <p className="text-gray-500 text-sm">
-                        {t('API Status: ')} {remainApiCount}
+                       API Status:  {remainApiCount}
                     </p>
                 </div>
             </div>
